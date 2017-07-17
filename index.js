1a0a33c9ba22618a91af0fad43706021be5abee7
Prima Aulia Gusta <primaulia@gmail.com>
2017-07-17 11:39:57 +0800

    latest with hbs

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
 const express = require('express')
 const app = express()
 
-// all request will have to know that the public folder is where we put our static files
-// public is referring to directory in the root folder
+// the package the has the handlebars logic
+const exphbs = require('express-handlebars')
+
+// public files setup
 app.use(express.static('public'))
+
+// setting dynamic view folders
+app.engine('handlebars', exphbs(
+  // handlebars configurations
+  {
+    defaultLayout: 'main',
+    partialsDir: 'views/partials'
+  }
+))
+app.set('view engine', 'handlebars')
 
 // listening for request
 app.get('/', function (req, res) {
-  res.send('index.html')
+  // this will be your search thru db
+  var user = {
+    name: 'Prima',
+    email: 'prima@ga.co',
+    isAdmin: true
+  }
+
+  var blogs = [
+    {
+      title: 'my blogpost',
+      content: 'here\'s my blogpost'
+    },
+    {
+      title: 'second blogpost',
+      content: 'here\'s my second blogpost'
+    }
+  ]
+
+  res.render('index', {
+    user,
+    blogs
+  })
 })
 
-app.get('/about/:name', function (req, res) {
-  res.send(req.params)
+app.get('/about', function (req, res) {
+
+  res.render('about')
 })
 
 app.get('/faq', function (req, res) {
-  res.send('faq page')
+  res.render('faq')
 })
-
-app.post('/')
 
 var port = process.env.PORT || 5000
 app.listen(port, function () {
